2ce389dd61bf56a1c658d61d5e3c97985a30e4d8
zhangfutang <futang_zhang@huace.cn>
2026-01-04 18:36:35 +0800

    update project builder for missing resources

--- project/linux/deploy.sh
+++ project/linux/deploy.sh
@@ -3,6 +3,9 @@
 cd ./bin/Release
 export PATH=/home/zft/Qt5.12.12/5.12.12/gcc_64/bin:$PATH
 cp ../../../../src/VeriQC_UI/Resources/Icons/VeriQC-LOGO.png ./default.png
+cp ../../../../src/VeriQC_UI/Resources/AlibabaPuHuiTi-3-55-Regular.ttf ./
+cp ../../../../src/VeriQC_UI/Resources/template.html ./
+cp ../../../../src/VeriQC_UI/Resources/VeriQCUserManual_zh.pdf ./
 cp ../../default.desktop ./
 linuxdeployqt default.desktop -executable-dir=. -appimage -qmake=/home/zft/Qt5.12.12/5.12.12/gcc_64/bin/qmake
 cp /lib/x86_64-linux-gnu/libcrypto.so.1.1 ./lib/
